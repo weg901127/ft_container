3400033cf4f044d34ca3a1bc4ae92d2a9e2ab558
gilee <gilee@student.42seoul.kr>
2022-05-15 22:16:40 +0900

    lol

---
+++ Google_tests/cmake-build-debug-/lib/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/gilee/CLionProjects/ft_container3/Google_tests/lib/googlemock
+# Build directory: /Users/gilee/CLionProjects/ft_container3/Google_tests/cmake-build-debug-/lib/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
---
+++ Google_tests/cmake-build-debug-/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/gilee/CLionProjects/ft_container3/Google_tests/lib/googletest
+# Build directory: /Users/gilee/CLionProjects/ft_container3/Google_tests/cmake-build-debug-/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
