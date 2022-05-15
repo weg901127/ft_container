e52100c2687a31e52a25ee1a59f4de36409a300d
gilee <weg901127@naver.com>
2022-05-15 21:29:03 +0900

    good

--- test_size.cpp
+++ test_size.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "Iterater.hpp"
+#include "iterater.hpp"
 #include <iterator>
 #include "vector.hpp"
 #include <vector>
@@ -54,13 +54,13 @@ TEST(vector, iter_test_with_args_2_container) {
 
 TEST(vector, iter_test_no_args) {
     EXPECT_DEATH({
-         ft::vector<int> a;
-         ft::vector<int>::iterator ia = a.begin();
-         std::cout << *ia << std::endl;
+        ft::vector<int> a;
+        ft::vector<int>::iterator ia = a.begin();
+        std::cout << *ia << std::endl;
         }, ".*");
     EXPECT_DEATH({
-         std::vector<int> b;
-         std::vector<int>::iterator ib = b.begin();
+        std::vector<int> b;
+        std::vector<int>::iterator ib = b.begin();
         std::cout << *ib << std::endl;
         }, ".*");
 }
@@ -95,4 +95,41 @@ TEST(vector, push_back_1) {
     std::vector<int>::iterator d = b.begin();
     EXPECT_EQ(*c++, *d++);
     EXPECT_EQ(*c, *d);
+}
+
+TEST(vector, resize_0) {
+    ft::vector<int> a(5, 10);
+    std::vector<int> b(5, 10);
+    b.resize(10);
+    a.resize(10);
+    EXPECT_EQ(a.size(), b.size());
+    EXPECT_EQ(a.capacity(), b.capacity());
+    b.resize(3);
+    a.resize(3);
+    EXPECT_EQ(a.size(), b.size());
+    EXPECT_EQ(a.capacity(), b.capacity());
+    b.resize(3092, 9292);
+    a.resize(3092, 9292);
+    EXPECT_EQ(a.size(), b.size());
+    EXPECT_EQ(a.capacity(), b.capacity());
+    ft::vector<int>::iterator c = a.begin();
+    std::vector<int>::iterator d = b.begin();
+    for (int i = 0; i < a.size(); i++) {
+        EXPECT_EQ(*c++, *d++);
+    }
+    b.resize(2, 3);
+    a.resize(2, 3);
+    for (int i = 0; i < a.size(); i++) {
+        EXPECT_EQ(*c++, *d++);
+    }
+}
+
+TEST(vector, pop_back_0) {
+    ft::vector<int> a(100);
+    std::vector<int> b(100);
+    a.pop_back();
+    b.pop_back();
+    std::cout << a.size() << " " << a.capacity() << std::endl;
+    std::cout << b.size() << " " << b.capacity() << std::endl;
+    std::cout << a.max_size() << " " << b.max_size() << std::endl;
 }
